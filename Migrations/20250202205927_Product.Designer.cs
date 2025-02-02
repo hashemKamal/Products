cea194fca3419046895abb931980b107b68f58ee
Hashem <hashemk078@gmail.com>
2025-02-02 23:10:09 +0200

    Commit 2

--- Migrations/20250202205927_Product.Designer.cs
+++ Migrations/20250202205927_Product.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductsCRUD.Data;
 
@@ -10,9 +11,11 @@ using ProductsCRUD.Data;
 namespace ProductsCRUD.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250202205927_Product")]
+    partial class Product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
